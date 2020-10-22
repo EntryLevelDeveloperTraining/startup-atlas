444740008d4d9b0f3fd9309e3075a7d37719afbf
Andrew Bliss <andrewgbliss@gmail.com>
2020-10-22 18:39:20 +0900

    updates

--- components/Sections/Cover/Cover.tsx
+++ components/Sections/Cover/Cover.tsx
@@ -1,37 +1,24 @@
-import { Transition } from '@headlessui/react';
-
 export default function Cover() {
   return (
     <div className="bg-banner-bk text-white text-center md:text-left">
-      <section>
-        <Transition
-          show={true}
-          appear={true}
-          enter="transition-opacity delay-500 duration-1000"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition-opacity duration-150"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="h-32" />
-          <h1 className="pady">
-            A New Way
-            <br />
-            To Start Business
-          </h1>
-          <p className="pady">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
-            dolor adipisci
-            <br />
-            dicta dolores qui quaerat iure cumque perspiciatis mollitia
-            accusamus ex aliquam et.
-          </p>
-          <div className="pady">
-            <button className="btn-primary">Get Started</button>
-          </div>
-          <div className="h-8 sm:h-16" />
-        </Transition>
+      <section className="fade-in">
+        <div className="h-32" />
+        <h1 className="pady">
+          A New Way
+          <br />
+          To Start Business
+        </h1>
+        <p className="pady">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora
+          dolor adipisci
+          <br />
+          dicta dolores qui quaerat iure cumque perspiciatis mollitia accusamus
+          ex aliquam et.
+        </p>
+        <div className="pady">
+          <button className="btn-primary">Get Started</button>
+        </div>
+        <div className="h-8 sm:h-16" />
       </section>
     </div>
   );
