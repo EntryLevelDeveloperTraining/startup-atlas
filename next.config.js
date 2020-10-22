5533459412422999e47982832a12ae9f97fcb9ac
Andrew Bliss <andrewgbliss@gmail.com>
2020-10-22 19:07:21 +0900

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
-module.exports = {
-  // Target must be serverless
-  target: 'serverless',
-};
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        /* config for next-optimized-images */
+      },
+    ],
+
+    // your other plugins here
+  ],
+  {
+    // Target must be serverless
+    target: 'serverless',
+  }
+);
