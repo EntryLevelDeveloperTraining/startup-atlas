f0a32ecbc85547d24e0e4c0a15b9bd317b5692da
Andrew Bliss <andrewgbliss@gmail.com>
2020-10-23 11:23:40 +0900

    updates

--- components/Layouts/Landing/Appbar.tsx
+++ components/Layouts/Landing/Appbar.tsx
@@ -2,7 +2,13 @@ import Nav from './Nav';
 
 export default function Appbar() {
   return (
-    <div className="absolute w-full">
+    <div
+      className="absolute w-full"
+      style={{
+        position: 'absolute',
+        width: '100%',
+      }}
+    >
       <section>
         <div className="flex justify-between items-center w-full sm:py-8">
           <img src="img/logo.png" className="w-20" />
